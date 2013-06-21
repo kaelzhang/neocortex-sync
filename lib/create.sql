dc6736cd7dbea92722dfb54bec1878aa484853b7
Kael <i@kael.me>
2013-06-21 20:01:09 +0800

    update sql for table CM_cortexPendingDependencies

--- lib/create.sql
+++ lib/create.sql
@@ -1,8 +1,21 @@
+-- create CM_cortexDependencies
 CREATE TABLE CM_cortexDependencies
 (
 Package varchar(50) not null,
 Version varchar(20) not null,
 Dependencies text,
-Pending text,
+IsPending tinyint not null,
 primary key (Package,Version)
+);
+
+
+-- create CM_cortexPendingDependencies
+CREATE TABLE CM_cortexPendingDependencies
+(
+Id int NOT NULL AUTO_INCREMENT,
+Package varchar(50) not null,
+Version varchar(20) not null,
+PendingPackage varchar(50) not null,
+PendingVersion varchar(20) not null,
+primary key (Id)
 );
